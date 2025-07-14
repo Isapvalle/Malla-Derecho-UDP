50cfa4726a7d365728f45fe85260b6c977a5fc35
Isapvalle <isapval04@gmail.com>
2025-07-14 16:19:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,77 @@
 const ramosPorSemestre = {
-  "1er Semestre": [
+  "1ER SEMESTRE": [
     { id: 1, nombre: "Fundamentos Histórico-Sociales del Derecho Privado", prerequisitos: [] },
     { id: 2, nombre: "Instituciones Políticas y Derechos Humanos", prerequisitos: [] },
     { id: 3, nombre: "Taller de Razonamiento Jurídico", prerequisitos: [] },
     { id: 4, nombre: "Taller de Investigación y Escritura Legal", prerequisitos: [] },
     { id: 5, nombre: "Comunicación Oral", prerequisitos: [] }
   ],
-  "2do Semestre": [
+  "2DO SEMESTRE": [
     { id: 6, nombre: "Persona y Acto Jurídico", prerequisitos: [1] },
     { id: 7, nombre: "Derecho Constitucional Orgánico", prerequisitos: [2] },
     { id: 8, nombre: "Introducción al Derecho I", prerequisitos: [3] },
-    { id: 9, nombre: "Bases Generales del Derecho Procesal", prerequisitos: [3] }
+    { id: 9, nombre: "Bases Generales del Derecho Procesal", prerequisitos: [3] },
+    { id: 10, nombre: "CFG 1", prerequisitos: [] },
+    { id: 11, nombre: "Inglés I", prerequisitos: [] },
   ],
-  "3er Semestre": [
-    { id: 10, nombre: "Derechos Reales y Derecho Inmobiliario", prerequisitos: [6] },
-    { id: 11, nombre: "Derechos Fundamentales", prerequisitos: [7] },
-    { id: 12, nombre: "Introducción al Derecho II", prerequisitos: [8] },
-    { id: 13, nombre: "Economía y Derecho", prerequisitos: [4] }
+  "3ER SEMESTRE": [
+    { id: 12, nombre: "Derechos Reales y Derecho Inmobiliario", prerequisitos: [6] },
+    { id: 13, nombre: "Derechos Fundamentales", prerequisitos: [7] },
+    { id: 14, nombre: "Introducción al Derecho II", prerequisitos: [8] },
+    { id: 15, nombre: "Economía y Derecho", prerequisitos: [4] },
+    { id: 16, nombre: "CFG 2", prerequisitos: [] },
+    { id: 17, nombre: "Inglés 2", prerequisitos: [11] },
   ],
-  "4to Semestre": [
-    { id: 14, nombre: "Derecho de las Obligaciones", prerequisitos: [10] },
-    { id: 15, nombre: "Derecho Penal Parte General I", prerequisitos: [12] },
-    { id: 16, nombre: "Taller Destrezas en Litigación Oral", prerequisitos: [5, 9, 10, 11, 12] },
-    { id: 17, nombre: "Derecho Regulatorio", prerequisitos: [13] }
+  "4TO SEMESTRE": [
+    { id: 18, nombre: "Derecho de las Obligaciones", prerequisitos: [12] },
+    { id: 19, nombre: "Derecho Penal Parte General I", prerequisitos: [14] },
+    { id: 20, nombre: "Taller Destrezas en Litigación Oral", prerequisitos: [5, 9, 12, 13, 14] },
+    { id: 21, nombre: "Derecho Regulatorio", prerequisitos: [15] },
+    { id: 22, nombre: "CFG 3", prerequisitos: [] },
   ],
-  "5to Semestre": [
-    { id: 18, nombre: "Derecho de Daños", prerequisitos: [15] },
-    { id: 19, nombre: "Derecho Internacional Público", prerequisitos: [11] },
-    { id: 20, nombre: "Derecho Penal Parte General II", prerequisitos: [12, 14] },
-    { id: 21, nombre: "Procedimientos Judiciales", prerequisitos: [16] },
-    { id: 22, nombre: "Derecho Comercial", prerequisitos: [17] }
+  "5TO SEMESTRE": [
+    { id: 23, nombre: "Derecho de Daños", prerequisitos: [18] },
+    { id: 24, nombre: "Derecho Internacional Público", prerequisitos: [13] },
+    { id: 25, nombre: "Derecho Penal Parte General II", prerequisitos: [14, 19] },
+    { id: 26, nombre: "Procedimientos Judiciales", prerequisitos: [20] },
+    { id: 27, nombre: "Derecho Comercial", prerequisitos: [21] },
+    { id: 28, nombre: "CFG 4", prerequisitos: [] },
   ],
-  "6to Semestre": [
-    { id: 23, nombre: "Contratos", prerequisitos: [4, 18] },
-    { id: 24, nombre: "Derecho Administrativo", prerequisitos: [19] },
-    { id: 25, nombre: "Derecho Penal Parte Especial", prerequisitos: [20] },
-    { id: 26, nombre: "Derecho Societario", prerequisitos: [22] }
+  "6TO SEMESTRE": [
+    { id: 29, nombre: "Contratos", prerequisitos: [4, 23] },
+    { id: 30, nombre: "Derecho Administrativo", prerequisitos: [20, 21] },
+    { id: 31, nombre: "Derecho Penal Parte Especial", prerequisitos: [25] },
+    { id: 32, nombre: "Derecho Societario", prerequisitos: [27] },
+    { id: 33, nombre: "CFG 5", prerequisitos: [] },
   ],
-  "7mo Semestre": [
-    { id: 27, nombre: "Derecho de Familia y Sucesorio", prerequisitos: [23] },
-    { id: 28, nombre: "Derecho y Medio Ambiente", prerequisitos: [24] },
-    { id: 29, nombre: "Proceso Penal", prerequisitos: [25] },
-    { id: 30, nombre: "Taller de Negociación en Procesos Colaborativos", prerequisitos: [21] },
-    { id: 31, nombre: "Derecho Tributario", prerequisitos: [17] }
+  "7MO SEMESTRE": [
+    { id: 34, nombre: "Derecho de Familia y Sucesorio", prerequisitos: [29] },
+    { id: 35, nombre: "Derecho y Medio Ambiente", prerequisitos: [30] },
+    { id: 36, nombre: "Proceso Penal", prerequisitos: [31] },
+    { id: 37, nombre: "Taller de Negociación en Procesos Colaborativos", prerequisitos: [26] },
+    { id: 38, nombre: "Derecho Tributario", prerequisitos: [21] },
+    { id: 39, nombre: "Derecho del Trabajo", prerequisitos: [15, 20] },
+    { id: 40, nombre: "CFG 6", prerequisitos: [] }
   ],
-  "8vo Semestre": [
-    { id: 32, nombre: "Clínicas Jurídicas (o Pasantía Supervisada)", prerequisitos: [27, 28, 29, 30, 31] },
-    { id: 33, nombre: "Curso Optativo I", prerequisitos: [27, 28, 29, 30, 31] },
-    { id: 34, nombre: "Curso Optativo II", prerequisitos: [27, 28, 29, 30, 31] },
-    { id: 35, nombre: "Curso Optativo III", prerequisitos: [27, 28, 29, 30, 31] }
+  
+  "8VO SEMESTRE": [
+    { id: 41, nombre: "Clínicas Jurídicas I (o Pasantía Supervisada)", prerequisitos: [34, 35, 36, 37] },
+    { id: 42, nombre: "Curso Optativo I", prerequisitos: [29, 30, 31, 32] },
+    { id: 43, nombre: "Curso Optativo II", prerequisitos: [29, 30, 31, 32] },
+    { id: 44, nombre: "Curso Optativo III", prerequisitos: [29, 30, 31, 32] }
   ],
-  "9no Semestre": [
-    { id: 36, nombre: "Clínicas Jurídicas (o Pasantía Supervisada)", prerequisitos: [32, 33, 34, 35] },
-    { id: 37, nombre: "Curso Optativo IV", prerequisitos: [32, 33, 34, 35] },
-    { id: 38, nombre: "Filosofía del Derecho", prerequisitos: [32, 33, 34, 35] },
-    { id: 39, nombre: "Seminario de Investigación y Escritura Jurídica Avanzada", prerequisitos: [32, 33, 34, 35] }
+      "9NO SEMESTRE": [
+    { id: 45, nombre: "Clínicas Jurídicas II (o Pasantía Supervisada)", prerequisitos: [34, 35, 36, 37] },
+    { id: 46, nombre: "Curso Optativo IV", prerequisitos: [29, 30, 31, 32] },
+    { id: 47, nombre: "Filosofía del Derecho", prerequisitos: [14] },
+    { id: 48, nombre: "Seminario de Investigación y Escritura Jurídica Avanzada", prerequisitos: [41] }
   ],
-  "10mo Semestre": [
-    { id: 40, nombre: "Curso de Integración y Actividad de Licenciatura", prerequisitos: [36, 37, 38, 39] },
-    { id: 41, nombre: "Ética y Responsabilidad Profesional en la Abogacía", prerequisitos: [36, 37, 38, 39] },
-    { id: 42, nombre: "Taller de Inserción y Emprendimiento Profesional", prerequisitos: [36, 37, 38, 39] },
-    { id: 43, nombre: "Curso Optativo V", prerequisitos: [36, 37, 38, 39] }
+  
+  "10MO SEMESTRE": [
+    { id: 49, nombre: "Curso de Integración y Actividad de Licenciatura", prerequisitos: [38, 39, 47, 48] },
+    { id: 50, nombre: "Ética y Responsabilidad Profesional en la Abogacía", prerequisitos: [45] },
+    { id: 51, nombre: "Taller de Inserción y Emprendimiento Profesional", prerequisitos: [45] },
+    { id: 52, nombre: "Curso Optativo V", prerequisitos: [29, 30, 31, 32] }
   ]
 };
 
